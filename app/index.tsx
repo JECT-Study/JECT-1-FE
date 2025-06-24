b35c2bf37acc538bd26605a790b7a8b96ac0a4d2
scorchedrice <wldnd2977@gmail.com>
2025-06-25 02:49:43 +0900

    design: 로고 중앙 배치

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import LoginCardSlider from "@/components/login/LoginCardSlider";
 import SocialLoginButtons from "@/components/login/SocialLoginButtons";
@@ -7,6 +7,12 @@ export default function Login() {
   return (
     <View className="flex-1 items-center bg-black px-4">
       <LoginCardSlider />
+      <View className="items-center">
+        <Text className="text-[34px] font-semibold text-white">마이코드</Text>
+        <Text className="text-[18px] text-white">
+          나에게 맞는 컨텐츠를 한눈에
+        </Text>
+      </View>
       <SocialLoginButtons />
     </View>
   );
