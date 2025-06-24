1335e9c3660321df942ae4b0cc13987073bda735
scorchedrice <wldnd2977@gmail.com>
2025-06-25 03:18:43 +0900

    design: 로고 중앙 배치

--- components/login/LoginMarquee.tsx
+++ components/login/LoginMarquee.tsx
@@ -24,7 +24,7 @@ export default function LoginMarquee({
           >
             <Image
               source={imageSrc}
-              style={{ width: "100%", height: "100%" }}
+              style={{ width: "100%", height: "100%", opacity: 0.3 }}
               contentFit="cover"
             />
           </View>
