c3e189dddffe80536e9e1e699cd3b8fe0107d40a
scorchedrice <wldnd2977@gmail.com>
2025-06-30 19:24:47 +0900

    design: MyMenus

--- components/mypage/MyMenu.tsx
+++ components/mypage/MyMenu.tsx
@@ -9,7 +9,7 @@ export default function MyMenu({ title, onPress }: Props) {
   return (
     <Pressable
       onPress={onPress}
-      className="m-2 flex h-[70px] w-[105px] items-center justify-center rounded-[8px] bg-[#D9D9D9]"
+      className="m-2 flex h-[70px] w-[105px] items-center justify-center"
     >
       <Text>{title}</Text>
     </Pressable>
--- components/mypage/MyMenus.tsx
+++ components/mypage/MyMenus.tsx
@@ -1,16 +1,35 @@
-import { View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
-import MyMenu from "@/components/mypage/MyMenu";
+import CalendarEditIcon from "@/components/icons/CalendarEditIcon";
+import DiaryIcon from "@/components/icons/DiaryIcon";
+import HeartIcon from "@/components/icons/HeartIcon";
 
 export default function MyMenus() {
   return (
-    <View className="my-4 flex flex-row justify-center">
-      <MyMenu title="나의일정" onPress={() => console.log("나의일정")} />
-      <MyMenu title="찜" onPress={() => console.log("나의일정")} />
-      <MyMenu
-        title="취향 분석"
-        onPress={() => console.log("취향 분석 다시하기")}
-      />
+    <View className="mx-6 my-4 flex flex-row items-center justify-center rounded-md bg-[#F2F3F6]">
+      <Pressable
+        onPress={() => console.log("Pressable")}
+        className="m-2 flex h-[70px] w-[105px] items-center justify-center"
+      >
+        <DiaryIcon />
+        <Text className="m-[3.25px] text-[13px]">나의일정</Text>
+      </Pressable>
+      <View aria-label="seperator" className="h-[20px] w-[1px] bg-[#DDDFE6]" />
+      <Pressable
+        onPress={() => console.log("Pressable")}
+        className="m-2 flex h-[70px] w-[105px] items-center justify-center"
+      >
+        <HeartIcon />
+        <Text className="m-[3.25px] text-[13px]">관심목록</Text>
+      </Pressable>
+      <View aria-label="seperator" className="h-[20px] w-[1px] bg-[#DDDFE6]" />
+      <Pressable
+        onPress={() => console.log("Pressable")}
+        className="m-2 flex h-[70px] w-[105px] items-center justify-center"
+      >
+        <CalendarEditIcon />
+        <Text className="m-[3.25px] text-[13px]">취향 분석하기</Text>
+      </Pressable>
     </View>
   );
 }
