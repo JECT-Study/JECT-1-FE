f5ce1a194fd56d1c79056201b8bd97f06ba6607e
scorchedrice <wldnd2977@gmail.com>
2025-07-01 00:02:24 +0900

    refactor : CustomHeader

--- app/my/withdrawal.tsx
+++ app/my/withdrawal.tsx
@@ -4,22 +4,17 @@ import { SafeAreaView, View, Text, Pressable } from "react-native";
 import Chevron from "@/components/icons/Chevron";
 import WithDrawForm from "@/components/mypage/WithDrawForm";
 import { reason } from "@/constants/WithDrawal";
+import CustomHeader from "@/components/ui/CustomHeader";
 
 export default function Withdrawal() {
   return (
     <>
       <SafeAreaView className="w-full flex-1 bg-white">
-        <View className="relative h-[60px] items-center justify-center border-b-[0.5px] border-[#DCDEE3]">
-          <Text className="text-[16px] font-medium text-[#383535]">
-            회원탈퇴
-          </Text>
-          <Pressable
-            className="absolute left-4 top-1/2 -translate-y-1/2"
-            onPress={() => router.replace("/(tabs)/my")}
-          >
-            <Chevron direction="left" />
-          </Pressable>
-        </View>
+        <CustomHeader
+          title="회원탈퇴"
+          isCommit={false}
+          cancel={() => router.replace("/(tabs)/my")}
+        />
         <Text className="m-6 text-[18px] font-medium">
           서비스를 탈퇴하는 이유가 궁금해요.
         </Text>
