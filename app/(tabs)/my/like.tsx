a1da667ada1b34c92b373857b6da37d9459d630c
scorchedrice <wldnd2977@gmail.com>
2025-07-01 00:23:23 +0900

    feat: like Filter

--- app/(tabs)/my/like.tsx
+++ app/(tabs)/my/like.tsx
@@ -1,9 +1,22 @@
-import { View, Text } from "react-native";
+import { router } from "expo-router";
+import { SafeAreaView, View } from "react-native";
+
+import LikeFilter from "@/components/mypage/LikeFilter";
+import CustomHeader from "@/components/ui/CustomHeader";
 
 export default function Like() {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <SafeAreaView className="w-full flex-1 items-center bg-white">
+      <CustomHeader
+        title="관심 목록"
+        isCommit={false}
+        cancel={() => {
+          router.replace("/my");
+        }}
+      />
+      <View className="flex w-full items-start p-6">
+        <LikeFilter />
+      </View>
+    </SafeAreaView>
   );
 }
