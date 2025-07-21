8d77aeef826594f44b991a4aa438e1bbafbaae47
scorchedrice <wldnd2977@gmail.com>
2025-07-22 02:44:39 +0900

    style: 텍스트 입력에 따른 ui 변화

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-import { SafeAreaView, TextInput, View } from "react-native";
+import { Pressable, SafeAreaView, TextInput, View } from "react-native";
 
 import Chevron from "@/components/icons/Chevron";
 import SearchIcon from "@/components/icons/SearchIcon";
 import RecentSearch from "@/components/search/RecentSearch";
+import XIcon from "@/components/icons/X";
 
 export default function SearchScreen() {
   const [searchText, setSearchText] = useState("");
@@ -12,15 +13,25 @@ export default function SearchScreen() {
     <SafeAreaView className="w-full flex-1 bg-white">
       <View className="flex flex-row items-center p-6">
         <Chevron direction={"left"} />
-        <View className="ml-3 flex h-11 flex-1 flex-row items-center rounded-full border-[1.2px] border-[#6B51FB] p-2">
-          <SearchIcon color="#6B51FB" />
+        <View className="ml-3 flex h-11 flex-1 flex-row items-center rounded-full border-[1.2px] border-[#6B51FB] p-3">
+          {searchText.length === 0 ? <SearchIcon color="#6B51FB" /> : null}
           <TextInput
-            className="px-4"
+            className="flex-1 px-4"
             placeholder="검색어를 입력해주세요."
             placeholderTextColor="#9CA3AF"
             value={searchText}
             onChangeText={(text) => setSearchText(text)}
           />
+          {searchText.length > 0 ? (
+            <Pressable
+              className="flex h-6 w-6 items-center justify-center rounded-full bg-[#8A8A8A]"
+              onPress={() => {
+                setSearchText("");
+              }}
+            >
+              <XIcon size={16} color="#FFFFFF" />
+            </Pressable>
+          ) : null}
         </View>
       </View>
       <RecentSearch textInput={searchText} setSearchText={setSearchText} />
