d45fbc608596659706f61156e7abc3d4ece5bdc2
scorchedrice <wldnd2977@gmail.com>
2025-07-22 02:09:36 +0900

    update: color

--- components/icons/X.tsx
+++ components/icons/X.tsx
@@ -1,13 +1,19 @@
 import Svg, { Path } from "react-native-svg";
 
-export default function XIcon({ size }: { size: number }) {
+export default function XIcon({
+  size,
+  color = "#111111",
+}: {
+  size: number;
+  color?: string;
+}) {
   return (
     <Svg
       width={size}
       height={size}
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke={color}
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
