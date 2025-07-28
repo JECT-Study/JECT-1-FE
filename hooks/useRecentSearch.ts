b7ddb1f979a1d6009d66669771ac82cfdb02e16e
scorchedrice <wldnd2977@gmail.com>
2025-07-28 16:09:11 +0900

    feat: 최근 검색어 api 연결

--- hooks/useRecentSearch.ts
+++ hooks/useRecentSearch.ts
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios, { AxiosError } from "axios";
+import { SearchRecentUrl } from "@/constants/ApiUrls";
 
 export default function useRecentSearch() {
   // TODO : 최근 검색어 가져오는 기능 구현 필요.
@@ -10,12 +12,27 @@ export default function useRecentSearch() {
     "8월 축제",
     "9월 축제",
   ]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(SearchRecentUrl);
+        // setRecentSearchData(response.result);
+      } catch (error) {
+        const axiosError = error as AxiosError;
+        console.error(axiosError);
+      }
+    };
+    // fetchData();
+  }, []);
+
   // TODO : 삭제하는 경우 서버에 api 요청을 보내야함.
-  const handleDelete = (keyword: string) => {
+  const handleDelete = async (keyword: string) => {
+    await axios.delete(`${SearchRecentUrl}/${keyword}`);
     setRecentSearchData((prev) => prev.filter((item) => item !== keyword));
   };
   const handleDeleteAll = () => {
     setRecentSearchData([]);
+    // TODO : 전체 삭제 관련 api가 없음. 설계되어야함.
   };
 
   return {
