063b625224841efdac40912d547c79fa87ab7fc4
scorchedrice <wldnd2977@gmail.com>
2025-08-05 23:13:02 +0900

    feat: 카카오 로그인 로직 구현

--- components/login/KakaoLogin.tsx
+++ components/login/KakaoLogin.tsx
@@ -1,26 +1,43 @@
 import { useEffect } from "react";
 
 import { initializeKakaoSDK } from "@react-native-kakao/core";
-import { login } from "@react-native-kakao/user";
+import { login, me } from "@react-native-kakao/user";
 import Constants from "expo-constants";
 import { Pressable, Text } from "react-native";
 
 import KakaoIcon from "@/components/icons/KakaoIcon";
+import axios, { AxiosError } from "axios";
+import { LoginUrl } from "@/constants/ApiUrls";
 
 export default function KakaoLogin() {
   const kakaoNativeAppKey =
     Constants.expoConfig?.extra?.kakaoNativeAppKey ?? "";
+
   useEffect(() => {
     initializeKakaoSDK(kakaoNativeAppKey);
   }, [kakaoNativeAppKey]);
 
-  // TODO : 추후 로그인 기능 관련 논의 필요
   async function kakaoLogin() {
     try {
-      const res = await login();
-      console.log(res);
+      await login();
+      // const accessToken = res.accessToken;
+      // const refreshToken = res.refreshToken;
     } catch (error) {
-      console.log(error);
+      alert("카카오 로그인 도중 에러가 발생했습니다.");
+    } finally {
+      const profile = await me();
+      const id = profile.id;
+      try {
+        await axios.post(LoginUrl, {
+          id,
+          social: "KAKAO",
+        });
+      } catch (error) {
+        const axiosError = error as AxiosError;
+        alert(
+          `로그인 정보를 서버로 전송하던 도중 에러가 발생했습니다. ${axiosError.message}`,
+        );
+      }
     }
   }
 
