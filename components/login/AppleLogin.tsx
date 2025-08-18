92096246cf538275bed778bcfa759342bc68d4f3
kimdonggu42 <kdk3280@undermilli.com>
2025-08-18 21:23:14 +0900

    feat: 로그인 된 상태에서 로그인 버튼들 비활성화 처리

--- components/login/AppleLogin.tsx
+++ components/login/AppleLogin.tsx
@@ -1,18 +1,26 @@
 import { Platform, Pressable, Text } from "react-native";
 
 import AppleIcon from "@/components/icons/AppleIcon";
-import { IOSAppleLogin, AndroidAppleLogin } from "@/features/auth/appleLogin";
+import { AndroidAppleLogin, IOSAppleLogin } from "@/features/auth/appleLogin";
 
-export default function AppleLogin() {
+interface AppleLoginProps {
+  disabled?: boolean;
+}
+
+export default function AppleLogin({ disabled = false }: AppleLoginProps) {
   const handlePress =
     Platform.OS === "android" ? AndroidAppleLogin : IOSAppleLogin;
+
   return (
     <Pressable
-      onPress={handlePress}
-      className="mx-auto w-full max-w-[500px] flex-row items-center justify-center gap-2 rounded-xl bg-white px-6 py-4 active:opacity-80"
+      onPress={disabled ? undefined : handlePress}
+      disabled={disabled}
+      className={`mx-auto h-14 w-full flex-row items-center justify-center gap-2 rounded-xl bg-white px-6 ${
+        disabled ? "opacity-50" : "active:opacity-80"
+      }`}
     >
       <AppleIcon size={20} color="black" />
-      <Text className="text-black">Apple로 시작하기</Text>
+      <Text className="text-lg font-medium text-black">Apple로 시작하기</Text>
     </Pressable>
   );
 }
--- components/login/KakaoLogin.tsx
+++ components/login/KakaoLogin.tsx
@@ -5,18 +5,27 @@ import { Pressable, Text } from "react-native";
 import KakaoIcon from "@/components/icons/KakaoIcon";
 import { initializeKakao, kakaoLogin } from "@/features/auth/kakaoLogin";
 
-export default function KakaoLogin() {
+interface KakaoLoginProps {
+  disabled?: boolean;
+}
+
+export default function KakaoLogin({ disabled = false }: KakaoLoginProps) {
   useEffect(() => {
     initializeKakao();
   }, []);
 
   return (
     <Pressable
-      onPress={kakaoLogin}
-      className="mx-auto w-full max-w-[500px] flex-row items-center justify-center gap-2 rounded-xl bg-yellow-300 px-6 py-4 active:opacity-80"
+      onPress={disabled ? undefined : kakaoLogin}
+      disabled={disabled}
+      className={`mx-auto h-14 w-full flex-row items-center justify-center gap-2 rounded-xl bg-[#F9DB00] px-6 ${
+        disabled ? "opacity-50" : "active:opacity-80"
+      }`}
     >
       <KakaoIcon size={20} color="#3E1918" />
-      <Text className="text-black">카카오톡으로 시작하기</Text>
+      <Text className="text-lg font-medium text-black">
+        카카오톡으로 시작하기
+      </Text>
     </Pressable>
   );
 }
