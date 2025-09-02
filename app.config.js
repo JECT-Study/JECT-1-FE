821cc19238d387832b22d1087b460e8a8ecfa787
kimdonggu42 <kdk3280@undermilli.com>
2025-09-02 20:03:07 +0900

    chore: buildnumber 설정

--- app.config.js
+++ app.config.js
@@ -15,6 +15,7 @@ export default {
     supportsTablet: true,
     usesAppleSignIn: true,
     bundleIdentifier: process.env.MYCODE_BUNDLE_IDENTIFIER,
+    buildNumber: "17",
     config: {
       usesNonExemptEncryption: false,
     },
