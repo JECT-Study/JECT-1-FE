569bb5a8f3345fc262dd1ecf3aa81636574cad5e
kimdonggu42 <kdk3280@undermilli.com>
2025-09-16 01:44:54 +0900

    chore: 빌드 넘버 변경

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ export default {
     supportsTablet: true,
     usesAppleSignIn: true,
     bundleIdentifier: process.env.MYCODE_BUNDLE_IDENTIFIER,
-    buildNumber: "19",
+    buildNumber: "20",
     backgroundColor: "#6B51FB",
     config: {
       usesNonExemptEncryption: false,
