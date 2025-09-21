20f6f5f71a28f046843f11e3e84a121f73c35499
kimdonggu42 <kdk3280@undermilli.com>
2025-09-21 22:47:35 +0900

    style: 마이페이지 디자인 수정

--- app/(tabs)/mypage.tsx
+++ app/(tabs)/mypage.tsx
@@ -3,7 +3,6 @@ import { useCallback, useState } from "react";
 import { AxiosError } from "axios";
 import { Image } from "expo-image";
 import { router, useFocusEffect } from "expo-router";
-import { dismissAll } from "expo-router/build/global-state/routing";
 import * as SecureStore from "expo-secure-store";
 import { setStatusBarStyle } from "expo-status-bar";
 import { Alert, Pressable, Text, View } from "react-native";
@@ -33,14 +32,7 @@ export default function MyScreen() {
     usePageNavigation();
 
   const handleAuthAction = async () => {
-    if (!isLoggedIn) {
-      // 로그인되지 않은 상태면 루트 페이지로 이동
-      dismissAll();
-      router.push("/");
-      return;
-    }
-
-    // 로그인된 상태면 로그아웃 확인 모달 표시
+    // 항상 로그아웃 확인 모달 표시
     setShowLogoutAlert(true);
   };
 
@@ -237,91 +229,120 @@ export default function MyScreen() {
         </Text>
       </View>
 
-      <View aria-label="user-info" className="ml-6 mt-7 flex h-[60px] flex-row">
-        <View className="size-[60px] overflow-hidden rounded-full">
-          {getProfileImageSource() ? (
-            <Image
-              source={getProfileImageSource()}
-              style={{ width: 60, height: 60 }}
+      {isLoggedIn ? (
+        <View>
+          <View
+            aria-label="user-info"
+            className="mt-4 flex h-[60px] flex-row px-4"
+          >
+            <View className="size-[60px] overflow-hidden rounded-full">
+              {getProfileImageSource() ? (
+                <Image
+                  source={getProfileImageSource()}
+                  style={{ width: 60, height: 60 }}
+                />
+              ) : (
+                <DefaultProfileIcon size={60} />
+              )}
+            </View>
+            <View className="ml-2 h-full justify-center p-2">
+              <Text className="mr-1 text-[16px]">{getDisplayName()}</Text>
+            </View>
+          </View>
+
+          <Pressable
+            className="mx-4 mt-4 flex h-10 items-center justify-center rounded-[4px] bg-gray-100"
+            onPress={handleEditProfile}
+          >
+            <Text className="text-sm font-medium text-[#424242]">
+              프로필 수정
+            </Text>
+          </Pressable>
+
+          <View className="mx-4 my-4 flex flex-row items-center justify-center rounded-md bg-[#F2F3F6]">
+            <Pressable
+              onPress={handlePlan}
+              className="m-2 flex h-[70px] w-[105px] items-center justify-center"
+            >
+              <DiaryIcon />
+              <Text className="mt-1.5 text-sm text-[#424242]">나의 일정</Text>
+            </Pressable>
+            <View
+              aria-label="seperator"
+              className="h-[20px] w-[1px] bg-[#DDDFE6]"
             />
-          ) : (
-            <DefaultProfileIcon size={60} />
-          )}
+            <Pressable
+              onPress={handleLike}
+              className="m-2 flex h-[70px] w-[105px] items-center justify-center"
+            >
+              <HeartIcon />
+              <Text className="mt-1.5 text-sm text-[#424242]">관심목록</Text>
+            </Pressable>
+            <View
+              aria-label="seperator"
+              className="h-[20px] w-[1px] bg-[#DDDFE6]"
+            />
+            <Pressable
+              onPress={handleSurvey}
+              className="m-2 flex h-[70px] w-[105px] items-center justify-center"
+            >
+              <CalendarEditIcon />
+              <Text className="mt-1.5 text-sm text-[#424242]">
+                취향 분석하기
+              </Text>
+            </Pressable>
+          </View>
         </View>
-        <View className="ml-2 h-full justify-center p-2">
-          <Text className="mr-1 text-[16px]">{getDisplayName()}</Text>
+      ) : (
+        <View className="px-5 py-4">
+          <View>
+            <Text className="mb-1 text-2xl font-semibold text-[#212121]">
+              내 코드에 딱 맞는 문화생활,
+            </Text>
+            <Text className="mb-3.5 text-2xl font-semibold text-[#212121]">
+              마이코드와 함께하세요!
+            </Text>
+            <Pressable
+              className="flex h-14 w-full items-center justify-center rounded-xl bg-[#6C4DFF] px-6"
+              onPress={() => {
+                router.dismissAll();
+                router.push("/");
+              }}
+            >
+              <Text className="text-center text-xl font-semibold text-white">
+                로그인 / 회원가입
+              </Text>
+            </Pressable>
+          </View>
         </View>
-      </View>
-
-      <Pressable
-        className="mx-6 mt-6 flex h-[32px] items-center justify-center rounded-[4px] bg-gray-100"
-        onPress={handleEditProfile}
-      >
-        <Text className="text-sm font-medium">프로필 수정</Text>
-      </Pressable>
-
-      {/* MyMenus 통합 */}
-      <View className="mx-6 my-4 flex flex-row items-center justify-center rounded-md bg-[#F2F3F6]">
-        <Pressable
-          onPress={handlePlan}
-          className="m-2 flex h-[70px] w-[105px] items-center justify-center"
-        >
-          <DiaryIcon />
-          <Text className="m-[3.25px] text-sm">나의일정</Text>
-        </Pressable>
-        <View
-          aria-label="seperator"
-          className="h-[20px] w-[1px] bg-[#DDDFE6]"
-        />
-        <Pressable
-          onPress={handleLike}
-          className="m-2 flex h-[70px] w-[105px] items-center justify-center"
-        >
-          <HeartIcon />
-          <Text className="m-[3.25px] text-sm">관심목록</Text>
-        </Pressable>
-        <View
-          aria-label="seperator"
-          className="h-[20px] w-[1px] bg-[#DDDFE6]"
-        />
-        <Pressable
-          onPress={handleSurvey}
-          className="m-2 flex h-[70px] w-[105px] items-center justify-center"
-        >
-          <CalendarEditIcon />
-          <Text className="m-[3.25px] text-sm">취향 분석하기</Text>
-        </Pressable>
-      </View>
+      )}
 
       <View
         aria-label="seperator"
         className="my-2 h-[12px] w-full bg-[#F2F2F7]"
       />
 
-      {/* MyPageMenus 내용 직접 구현 */}
       <View className="w-full px-4">
         <Pressable
           onPress={() => goTerms()}
-          className="flex h-[50px] w-full flex-row items-center justify-between border-b-[1px] border-[#E5E5EC]"
+          className="flex h-16 w-full flex-row items-center justify-between border-b-[1px] border-[#E5E5EC]"
         >
           <Text className="text-base">이용약관</Text>
           <NewChevronRight />
         </Pressable>
-        <Pressable
-          onPress={handleAuthAction}
-          className={`flex h-[50px] w-full flex-row items-center justify-between ${
-            isLoggedIn ? "border-b-[1px] border-[#E5E5EC]" : ""
-          }`}
-        >
-          <Text className="text-base">
-            {isLoggedIn ? "로그아웃" : "로그인"}
-          </Text>
-          <NewChevronRight />
-        </Pressable>
+        {isLoggedIn && (
+          <Pressable
+            onPress={handleAuthAction}
+            className="flex h-16 w-full flex-row items-center justify-between border-b-[1px] border-[#E5E5EC]"
+          >
+            <Text className="text-base">로그아웃</Text>
+            <NewChevronRight />
+          </Pressable>
+        )}
         {isLoggedIn && (
           <Pressable
             onPress={() => goWithdrawal()}
-            className="flex h-[50px] w-full flex-row items-center justify-between"
+            className="flex h-16 w-full flex-row items-center justify-between"
           >
             <Text className="text-base">회원탈퇴</Text>
             <NewChevronRight />
--- components/icons/HeartIcon.tsx
+++ components/icons/HeartIcon.tsx
@@ -1,15 +1,51 @@
-import Svg, { Path } from "react-native-svg";
+import Svg, { Path, Defs, Filter, FeFlood, FeColorMatrix, FeOffset, FeGaussianBlur, FeComposite, FeBlend, G } from "react-native-svg";
 
 export default function HeartIcon() {
   return (
-    <Svg width="25" height="24" viewBox="0 0 25 24" fill="none">
-      <Path
-        d="M12.5 20L4.96368 12.9671C3.01211 11.1449 3.01211 8.18884 4.96368 6.36664C6.91526 4.54445 10.0842 4.54445 12.0358 6.36664L12.5 6.79903L12.9642 6.36664C14.9158 4.54445 18.0847 4.54445 20.0363 6.36664C21.9879 8.18884 21.9879 11.1449 20.0363 12.9671L12.5 20Z"
-        stroke="#424242"
-        strokeWidth="1.6"
-        strokeMiterlimit="10"
-        strokeLinejoin="round"
-      />
+    <Svg width="27" height="25" viewBox="0 0 27 25" fill="none">
+      <Defs>
+        <Filter
+          id="filter0_d_2116_4589"
+          x="0.45"
+          y="0.471484"
+          width="26.1"
+          height="24.0561"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <FeFlood floodOpacity="0" result="BackgroundImageFix" />
+          <FeColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <FeOffset />
+          <FeGaussianBlur stdDeviation="0.65" />
+          <FeComposite in2="hardAlpha" operator="out" />
+          <FeColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.186223 0 0 0 0 0.167135 0 0 0 0 0.167135 0 0 0 0.21 0"
+          />
+          <FeBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_2116_4589"
+          />
+          <FeBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_2116_4589"
+            result="shape"
+          />
+        </Filter>
+      </Defs>
+      <G filter="url(#filter0_d_2116_4589)">
+        <Path
+          d="M22.1044 2.97035C21.0023 2.19448 19.6608 1.77405 18.2792 1.77148C17.1 1.78302 15.9492 2.12354 14.9734 2.74969C14.3774 3.13144 13.8386 3.58677 13.3719 4.1033C13.1317 3.89527 12.8689 3.67306 12.5798 3.43512C11.3722 2.43271 9.81423 1.87929 8.19732 1.87839C7.38671 1.8818 6.58479 2.04329 5.84249 2.3526C5.10019 2.66191 4.43374 3.11229 3.88544 3.67513C1.42329 6.19819 1.08012 9.74917 2.90867 13.7074C4.10398 16.3034 6.56875 18.6697 8.42567 20.2005C9.71147 21.2586 11.3127 22.4697 12.4759 22.9817L12.4675 22.9981C12.7578 23.1516 13.0865 23.2306 13.4206 23.2269C13.591 23.2336 13.7614 23.21 13.9228 23.1575C14.0393 23.1274 14.1523 23.0861 14.2599 23.0344C14.8766 22.7585 15.9283 22.0932 17.8736 20.4866C19.9485 18.8112 21.7836 16.8907 23.3325 14.7735C24.3143 13.3027 24.9484 11.6527 25.1917 9.93542C25.3595 8.62024 25.1636 7.28852 24.6229 6.0686C24.0821 4.84867 23.2148 3.78166 22.1044 2.97035Z"
+          fill="#6C4DFF"
+        />
+      </G>
     </Svg>
   );
 }
